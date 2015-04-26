5deedf4498286530f2c2d00a3d8436994b9a58ec
alamarre_2 <a.lamarre@gmail.com>
2015-04-26 20:32:03 +0000

    hello world

---
+++ HelloWorld/HelloWorld.jsx
@@ -0,0 +1,21 @@
+define(['react',"utilities/ModuleManager"],
+    function(React, moduleManager) {
+        var HelloWorld = React.createClass({
+            componentDidMount: function() {
+                var url = this.props.url;
+                moduleManager.setSaveFunction(this.getDOMNode(),function() {
+                    return {"omnyClass":"HelloWorld","data":{}};
+                }, url);
+            },
+          render: function() {
+            return <div>Hello World!</div>;
+          }
+        });
+        function Hello(data, editable, url) {
+            this.render = function(element) {
+                React.render(<HelloWorld url={url} />, element)
+            }
+        }
+        return Hello;
+    }
+);
